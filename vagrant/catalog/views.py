2134896b78a35960efc453ef3e60aa81d71053b6
Maxwell Vaughn <mavaughn07@gmail.com>
2019-01-26 16:04:20 -0500

    add item viewable on category page when logged in

--- vagrant/catalog/views.py
+++ vagrant/catalog/views.py
@@ -41,15 +41,18 @@ APPLICATION_NAME = "Udacity Catalog App"
 @auth.verify_password
 def verify_password(username_or_token, password):
 	#Try to see if it's a token first
-	user_id = User.verify_auth_token(username_or_token)
-	if user_id:
-		user = session.query(User).filter_by(id = user_id).one()
-	else:
-		user = session.query(User).filter_by(username = username_or_token).first()
-		if not user or not user.verify_password(password):
-			return False
-	g.user = user
-	return True
+	# user_id = User.verify_auth_token(username_or_token)
+	# if user_id:
+	# 	user = session.query(User).filter_by(id = user_id).one()
+	# else:
+	# 	user = session.query(User).filter_by(username = username_or_token).first()
+	# 	if not user or not user.verify_password(password):
+	# 		return False
+	# g.user = user
+	# return True
+	if 'username' in login_session:
+		return True
+	return False
 
 # Create anti-forgery state token
 @app.route('/login', methods=['GET'])
