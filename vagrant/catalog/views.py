98fa24a2d286856a01ba68e6c4aff92b30219ac1
Maxwell Vaughn <mavaughn07@gmail.com>
2019-01-26 18:56:51 -0500

    removed testing print statements

--- vagrant/catalog/views.py
+++ vagrant/catalog/views.py
@@ -91,7 +91,6 @@ def gconnect():
 	if result['issued_to'] != CLIENT_ID:
 		response = make_response(
 			json.dumps("Token's client ID does not match app's."), 401)
-		print("Token's client ID does not match app's.")
 		response.headers['Content-Type'] = 'application/json'
 		return response
 
@@ -113,7 +112,6 @@ def gconnect():
 	answer = requests.get(userinfo_url, params=params)
 
 	data = answer.json()
-	print(data)
 	if 'name' in data:
 		login_session['username'] = data['name']
 	else:
@@ -136,19 +134,13 @@ def gconnect():
 @app.route('/gdisconnect')
 def gdisconnect():
 	access_token = login_session['access_token']
-	print('In gdisconnect access token is %s', access_token)
-	print('User name is: ')
-	print(login_session['username'])
 	if access_token is None:
-		print('Access Token is None')
 		response = make_response(json.dumps('Current user not connected.'), 401)
 		response.headers['Content-Type'] = 'application/json'
 		return response
 	url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
 	h = httplib2.Http()
 	result = h.request(url, 'GET')[0]
-	print('result is ')
-	print(result)
 	if result['status'] == '200':
 		del login_session['access_token']
 		del login_session['gplus_id']
@@ -176,7 +168,6 @@ def viewCategories():
 	return render_template('categories.html', categories=categories, latestItems=items)
 
 @app.route('/catalog/<string:cat_name>')
-@app.route('/catalog/<string:cat_name>/items')
 def viewIndividual(cat_name):
 	#this is the page that will view all items inside a specific category
 
@@ -194,7 +185,7 @@ def viewDescription(cat_name,item_name):
 
 	return render_template('item.html', item = item)
 
-@app.route('/catalog/<string:cat_name>/create', methods=['GET', 'POST'])
+@app.route('/create/<string:cat_name>', methods=['GET', 'POST'])
 @auth.login_required
 def itemCreate(cat_name):
 	#this page will be for creating an ITEM
